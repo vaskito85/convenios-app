c05e8cc41d9ec4a83110a771a42450c52be84ae6
vaskito85 <105981708+vaskito85@users.noreply.github.com>
2026-02-11 14:00:48 -0300

    Update firebase_init.py
    mejoras

--- firebase_init.py
+++ firebase_init.py
@@ -1,14 +1,19 @@
-import json, streamlit as st, firebase_admin
+import json
+import streamlit as st
+import firebase_admin
 from firebase_admin import credentials
 from google.cloud import firestore
 
 def init_firebase():
-    if "firebase_initialized" in st.session_state:
+    # Si ya existe la app de Firebase, NO volver a inicializarla
+    if firebase_admin._apps:
         return
+
     cred_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
     cred = credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred, {"projectId": st.secrets["FIREBASE_PROJECT_ID"]})
-    st.session_state["firebase_initialized"] = True
+    firebase_admin.initialize_app(cred, {
+        "projectId": st.secrets["FIREBASE_PROJECT_ID"]
+    })
 
 def get_db() -> firestore.Client:
     return firestore.Client(project=st.secrets["FIREBASE_PROJECT_ID"])
